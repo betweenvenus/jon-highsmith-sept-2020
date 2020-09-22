8e6ee3d40c824916461b8d547f2c6fb3abd39001
Jon Highsmith <jon@gojira.dev>
2020-09-22 08:10:25 -0400

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<style>
+	.is-this-obnoxious\? {
+		font-size: 5rem;
+	}
+</style>
+
+<svelte:head>
+	<title>Jon Highsmith</title>
+</svelte:head>
+
+<h1 class="is-this-obnoxious?">
+	Hi, I'm Jon Highsmith.
+</h1>
