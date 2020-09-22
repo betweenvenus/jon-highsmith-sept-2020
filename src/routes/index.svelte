7abf3f7700dfd984284fc177e5199fccc68214fe
Jon Highsmith <jon@jonhighsmith.com>
2020-09-22 16:25:04 -0400

    THE DISC

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,11 +6,14 @@
 <style>
   .is-this-obnoxious\? {
     font-size: 5rem;
+    mix-blend-mode: difference;
+    color: white;
   }
 
   .probably\,.if.i.had.to.guess {
     font-size: 3rem;
   }
+
 </style>
 
 <svelte:head>
@@ -20,4 +23,8 @@
 <h1 class="is-this-obnoxious?">Hi there :-)</h1>
 <h2 class="probably, if i had to guess">I'm Jon!!!</h2>
 
-{@html intro.html}
+{#if intro}
+  {@html intro.html}
+{/if}
+
+<img src="/img/the_compact_disc(光盤).png" alt="">
