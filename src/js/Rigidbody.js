d35899a44276f5f6f8e1ca5e4f6270671c5bba36
Jon Highsmith <jon@gojira.dev>
2020-10-05 18:22:14 -0400

    new constructor for rigidbody because i'm dumb

--- src/js/Rigidbody.js
+++ src/js/Rigidbody.js
@@ -4,7 +4,16 @@
  */
 
  class Rigidbody {
-     constructor() {
-         //
+     constructor({body}) {
+         if(body) {
+            this.position = body.position;
+            this.linearVelocity = body.linearVelocity;
+            this.angle = body.angle;
+            this.angularVelocity = body.angularVelocity;
+            this.force = body.force;
+            this.torque = body.torque;
+            this.torque = body.torque;
+            this.shape = body.shape; // shape should be of type BoxShape
+         }
      }
  }
